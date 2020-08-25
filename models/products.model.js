1dccb546a172b0181af6d249b61374d874e9457c
Filip Cwiek <fifek.inex@gmail.com>
2020-08-25 14:38:30 +0200

    Add department, products, employees models.js

---
+++ models/products.model.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const productsSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  client: { type: String, required: true }
+});
+module.exports = mongoose.model('Products', productsSchema);
